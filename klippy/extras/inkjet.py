032697258e20071f753c3c3caae477acb36d5e3a
ExplorerLGD <you@example.com>
2024-06-07 15:35:12 +0000

    cb1

--- klippy/extras/inkjet.py
+++ klippy/extras/inkjet.py
@@ -1,11 +1,8 @@
 import logging,os
 import sys,optparse, time
 import can
-
-
-#tail -f ~/printer_data/logs/klippy.log
+import struct
 #cat /sys/class/thermal/thermal_zone0/temp
-#~/klippy-env/bin/pip install -v bitarray
 
 class Inkjet:
     def __init__(self,config):
@@ -13,99 +10,72 @@ class Inkjet:
         self.reactor=self.printer.get_reactor()
         self.gcode=self.printer.lookup_object('gcode')
         #--can bus-----
-        self.tp_state=False
-        self.INK_TP_ID = 0x44C
-        self.ink_tp_bus=None
-        self.ink_tp_recv_bus=None
-        #self.ink_pressure_bus=None
-        #----tp setting-------
-        self.ink_temperature=0
-        self.ink_pressure=0
-        self.ink_temperature_set=0
-        self.ink_pressure_set=0
+        self.INK_SUPPLY_CTL_ID = 0x44C
+        self.INK_UV_LIGHT_CTL_ID = 0x44E
+        #self.ink_tp_filters = [{"can_id": self.INK_SUPPLY_CTL_ID, "can_mask": 0x7ff,"extended": False}]
+        #self.inkjet_bus = can.interface.Bus(channel="can0", bitrate=500000, can_filters=self.ink_tp_filters, bustype='socketcan')
+        self.inkjet_bus = can.interface.Bus(channel="can0", bitrate=500000,  bustype='socketcan')
+        #-----控制参数-----------
+        self.ink_heat_PWM=0
+        self.ink_pump_pwm=0
+        self.uv_light_en=0
+        self.uv_fan_en=0
         #----------
         self.gcode.register_command('CANTEST',self.cmd_cantest)
-        self.gcode.register_command('TP_ON',self.cmd_TP_ON)
-        self.gcode.register_command('TP_OFF',self.cmd_TP_OFF)
-        self.gcode.register_command('SET_INK_TEMP',self.cmd_SET_INK_TEMP)
-        self.gcode.register_command('SET_INK_PRESS',self.cmd_SET_INK_PRESS)
-        self._InitCAN()
+        self.gcode.register_command('INK_HEAT_PWM',self.cmd_INK_HEAT_PWM)
+        self.gcode.register_command('INK_PUMP_PWM',self.cmd_INK_PUMP_PWM)
+        self.gcode.register_command('UV_LIGHT_EN',self.cmd_UV_LIGHT_EN)
+        self.gcode.register_command('UV_FAN_EN',self.cmd_UV_FAN_EN)
+        #self._InitCAN()
 
-        
-    def _InitCAN(self):
-        ink_tp_filters = [{"can_id": self.INK_TP_ID, "can_mask": 0x7ff,"extended": False}]
-        self.ink_tp_bus = can.interface.Bus(channel="can0",bitrate=500000, can_filters=ink_tp_filters,bustype='socketcan')
-        ink_tp_recv_filters = [{"can_id": self.INK_TP_ID+1, "can_mask": 0x7ff,"extended": False}]
-        self.ink_tp_recv_bus = can.interface.Bus(channel="can0",bitrate=500000, can_filters=ink_tp_recv_filters,bustype='socketcan')
+    def _send_ink_supply_ctl_data(self):
+        byte_array = struct.pack('HH', self.ink_heat_PWM, self.ink_pump_pwm)
+        msg = can.Message(arbitration_id=self.INK_SUPPLY_CTL_ID,data=byte_array, is_extended_id=False)
+        self.inkjet_bus.send(msg)
+
+    def cmd_INK_HEAT_PWM(self,gcmd):
+        pwm = gcmd.get_int('VALUE', 0, minval=0, maxval=255)
+        self.ink_heat_PWM=pwm
+        self._send_ink_supply_ctl_data()
+        self.gcode.respond_info("Ink heat pwm: %s"%(self.ink_heat_PWM))
 
 
-    def _tp_callback(self,eventtime):
-        data=self.ink_tp_recv_bus.recv(0.3)
-        if data is not None:
-            self.gcode.respond_info("TP data:%s"%(data))
-        if(self.tp_state):
-            next_time=self.reactor.monotonic()+1
-            self.reactor.register_callback(self._tp_callback, next_time)
+    def cmd_INK_PUMP_PWM(self,gcmd):
+        pwm = gcmd.get_int('VALUE', 0, minval=0, maxval=4096)
+        self.ink_pump_pwm=pwm
+        self._send_ink_supply_ctl_data()
+        self.gcode.respond_info("Ink pump pwm: %s"%(self.ink_pump_pwm))
 
-    def cmd_TP_ON(self,gcmd):
-        string="TP ON   "
-        bytes_data = string.encode()
-        byte_array = bytearray(bytes_data)
-        msg = can.Message(arbitration_id=self.INK_TP_ID,data=byte_array, is_extended_id=False)
-        self.ink_tp_bus.send(msg)
-        self.tp_state=True
-        self.reactor.register_callback(self._tp_callback)
+    def _send_uv_light_ctl_data(self):
+        byte_array = struct.pack('HH', self.uv_light_en, self.uv_fan_en)
+        msg = can.Message(arbitration_id=self.INK_UV_LIGHT_CTL_ID,data=byte_array, is_extended_id=False)
+        self.inkjet_bus.send(msg)
 
-    def cmd_TP_OFF(self,gcmd):
-        string="TP OFF  "
-        bytes_data = string.encode()
-        byte_array = bytearray(bytes_data)
-        msg = can.Message(arbitration_id=self.INK_TP_ID,data=byte_array, is_extended_id=False)
-        self.ink_tp_bus.send(msg)
-        self.tp_state=False
+    def cmd_UV_LIGHT_EN(self,gcmd):
+        en = gcmd.get_int('VALUE', 0, minval=0, maxval=1)
+        self.uv_light_en=en
+        self._send_uv_light_ctl_data()
+        self.gcode.respond_info("UV light state: %s"%(self.uv_light_en))
 
-    def cmd_SET_INK_TEMP(self,gcmd):
-        value = gcmd.get('VALUE')
-        self.ink_temperature_set=value
-        self._send_tp_setting()
-    
-    def cmd_SET_INK_PRESS(self,gcmd):
-        value = gcmd.get('VALUE')
-        self.ink_pressure_set=value
-        self._send_tp_setting()
+    def cmd_UV_FAN_EN(self,gcmd):
+        en = gcmd.get_int('VALUE', 0, minval=0, maxval=1)
+        self.uv_fan_en=en
+        self._send_uv_light_ctl_data()
+        self.gcode.respond_info("UV fan state: %s"%(self.uv_fan_en))
 
-    def _send_tp_setting(self):
-        self.gcode.respond_info("temp:%s pressure:%s"%(self.ink_temperature_set,self.ink_pressure_set))
-        string='{:0>4}{:0>4}'.format(self.ink_temperature_set, self.ink_pressure_set)
-        #string=str(self.ink_temperature)+str(self.ink_pressure)
-        bytes_data = string.encode()
-        byte_array = bytearray(bytes_data)
-        # for i in byte_array:
-        #     self.gcode.respond_info("can send msg:%s"%(i))
-        msg = can.Message(arbitration_id=self.INK_TP_ID,data=byte_array, is_extended_id=False)
-        self.ink_tp_bus.send(msg)
+
 
     def cmd_cantest(self,gcmd):
-        self.cmd_InitCAN(gcmd)
-        string = "ISEN    "
-        bytes_data = string.encode()
-        byte_array = bytearray(bytes_data)
-        msg = can.Message(arbitration_id=self.INK_TP_ID,data=byte_array, is_extended_id=False)
-        self.ink_tp_bus.send(msg)
+        byte_array = struct.pack('BH', self.ink_heat_PWM, self.ink_pump_pwm)
+        self.gcode.respond_info("pack data.")
+        msg = can.Message(arbitration_id=self.INK_SUPPLY_CTL_ID,data=byte_array, is_extended_id=False)
+        self.inkjet_bus.send(msg)
         # self.gcode.respond_info("can send msg")
         # msg=bus.recv(1)
         # if msg is not None:
         #     self.gcode.respond_info("can recv msg:%s"%(msg.data))
         # else:
         #     self.gcode.respond_info("can recv None")
-
-
-
-
-
-
-
-
 
 
 def load_config(config):
--- klippy/extras/xaar1003.py
+++ klippy/extras/xaar1003.py
@@ -1,9 +1,12 @@
+    #创建发送数据，高四位为1时发送控制数据，为0时发送打印数据       
+    #控制信号DATA,从高到低 待定 待定 喷墨使能 主使能
 import logging,os
 import time
 import socket
 import queue
 import threading
 import serial
+import multiprocessing
 
 #tail -f ~/printer_data/logs/klippy.log
 #cat /sys/class/thermal/thermal_zone0/temp
@@ -15,19 +18,21 @@ class Xaar1003:
         self.reactor=self.printer.get_reactor()
         self.gcode=self.printer.lookup_object('gcode')
         self.virtual_sdcard=self.printer.lookup_object("virtual_sdcard")
-        #---------------通信--------------------
-        self.cmd_socket = None
-        self.data_socket = None
-        self.test_thread=None
-        self.print_data_transfer=False
+        #---------------数据通信--------------------
+        #self.ser=serial.Serial(port='/dev/ttyCH9344USB3',baudrate=6000000,bytesize=serial.FIVEBITS,parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE,timeout=0,)
+        self.cmd_queue = multiprocessing.Queue()
+        self.data_queue = multiprocessing.Queue()
+        self.process_exit=multiprocessing.Value('i', 0)
+        self.parent_conn, self.child_conn = multiprocessing.Pipe()
+
         #----------控制状态--------------------
-        self.head_en="0"
-        self.jet_en="0"
-        self.print_done=False
-        self.fifo_full=False    
+        self.head_en=False
+        self.jet_en=False
+        self.print_done=False  
         #---------gcode------------------- 
         self.gcode.register_command('PRINT_INIT',self.cmd_PRINT_INIT)
         self.gcode.register_command('LOAD_CONFIG',self.cmd_LOAD_CONFIG)
+        self.gcode.register_command('CREAT_PROCESS',self.cmd_CREAT_PROCESS)
         self.gcode.register_command('LOAD_PRINT_DATA',self.cmd_LOAD_PRINT_DATA)
 
         self.gcode.register_command('JET_ENABLE',self.cmd_JET_ENABLE)
@@ -38,95 +43,179 @@ class Xaar1003:
         self.gcode.register_command('TEST2',self.cmd_TEST2)
         self.gcode.register_command('TEST3',self.cmd_TEST3)
 
-    #创建发送数据，高四位为1时发送控制数据，为0时发送打印数据       
-    #控制信号DATA,从高到低 待定 待定 喷墨使能 主使能
 
-    #喷头驱动主使能 禁用
-    def cmd_HEAD_DISABLE(self,gcmd):
-        if not self.cmd_socket==None:
-            if not self.cmd_socket._closed:
-                self.cmd_socket.send(b'\x10')
-                self.cmd_socket.close()
-        if not self.data_socket==None:
-            if not self.data_socket._closed:
-                self.data_socket.close()  
-        self.print_done=True
-        self.head_en="0"   
-        self.jet_en="0"        
-        time.sleep(0.5)
-        self.gcode.respond_info("Head Disable")
-    #喷墨使能 启用
-    def cmd_JET_ENABLE(self,gcmd):
-        self.cmd_socket.send(b'\x13')  
-        self.jet_en="1" 
-        self.gcode.respond_info("Jet Enable")
-    #喷墨使能 禁用
-    def cmd_JET_DISABLE(self,gcmd):
-        self.cmd_socket.send(b'\x11')  
-        self.jet_en="0"    
-        self.gcode.respond_info("Jet Disable") 
+
+        #初始化
+    def cmd_PRINT_INIT(self,gcmd):
+        self.print_done=False
+        self.head_en=False
+        self.jet_en=False
+        self._clear_queue(self.data_queue)
+        self._clear_queue(self.cmd_queue)
+        with self.process_exit.get_lock():
+            self.process_exit.value=0
+        process = multiprocessing.Process(target=self._serial_process, args=(self.cmd_queue, self.data_queue,self.process_exit,))
+        process.daemon=True
+        process.start()
+        self.gcode.respond_info("Process created.")      
+        self.gcode.respond_info("Init finish.")
 
     def cmd_LOAD_CONFIG(self,gcmd):
         PATH=gcmd.get('PATH')
         config_file_path = os.path.join(self.virtual_sdcard.sdcard_dirname, PATH)
-        self.gcode.respond_info("Loading config file:%s"%(config_file_path))
+        self.gcode.respond_info("Loading config file:%s"%(config_file_path)) 
+        retry_count=0
+        while True:
+            try:
+                #发送一个重置信号
+                self.cmd_queue.put(b'\x11')
+                self.cmd_queue.put(b'\x10')
+                time.sleep(0.5)
+                self.cmd_queue.put(b'\x11')
+                time.sleep(0.5)
+                self.gcode.respond_info("Config data loading...")
 
-        #发送一个重置信号
-        self.cmd_socket.send(b'\x11')
-        self.cmd_socket.send(b'\x10')
-        time.sleep(0.5)
-        self.cmd_socket.send(b'\x11')
-        time.sleep(0.5)
-        #设置主使能状态
-        self.head_en="1"  
-        #读取文件并发送数据
-        with open(config_file_path,"r") as file:
-            content=file.read()
-            self.data_socket.sendall(content.encode('utf-8'))
-        time.sleep(2)
-        self.gcode.respond_info("Load config done")
-
-    #初始化
-    def cmd_PRINT_INIT(self,gcmd):
-        self.print_done=False
-        self.fifo_full=False
-        self.head_en="0"
-        self.jet_en="0"
-        self.cmd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        self.cmd_socket.connect(("10.10.10.3",20001))
-        self.data_socket.connect(("10.10.10.3",20000))
-        self.gcode.respond_info("Init finish.")
-    #将一层打印数据通过网口发送
+                #读取文件并发送数据
+                with open(config_file_path,"r") as file:
+                    content=file.read()
+                    while content:
+                        chunk = content[:64]
+                        hex_data=[int(s,16) for s in chunk]
+                        bytes_data=bytes(hex_data)
+                        self.data_queue.put(bytes_data)
+                        content = content[64:] 
+                        #读取返回信息，从高到低为: 0 满信号 tx校验 rx校验，如果是正确的校验，则返回的值应该是0
+                        while self.parent_conn.poll():
+                            rev=self.parent_conn.recv()
+                            if rev!=0:
+                                raise gcmd.error("Config data error,auto try agin...") 
+                time.sleep(0.5)
+                self.gcode.respond_info("Load config done")
+                return
+            except:
+                retry_count += 1
+                if retry_count >= 10:
+                    raise gcmd.error("Config max retry count exceeded,Please try agin later.")
+                time.sleep(1)   
+    
+        #将一层打印数据加载到data_queue
+    
+    def cmd_CREAT_PROCESS(self,gcmd):
+        with self.process_exit.get_lock():
+            self.process_exit.value=0
+        process = multiprocessing.Process(target=self._serial_process, args=(self.cmd_queue, self.data_queue,self.process_exit,))
+        process.daemon=True
+        process.start()
+        self.gcode.respond_info("Process created.")
+    
     def cmd_LOAD_PRINT_DATA(self,gcmd):
         PATH=gcmd.get('PATH')
         data_file_path = os.path.join(self.virtual_sdcard.sdcard_dirname, PATH)
+        self.data_trans_en=True
         with open(data_file_path,"r") as file:
             content=file.read()
-            self.data_socket.sendall(content.encode('utf-8'))
-            self.gcode.respond_info("Load print data done.")
+            while content:
+                    chunk = content[:512]
+                    hex_data=[int(s,16) for s in chunk]
+                    bytes_data=bytes(hex_data)
+                    self.data_queue.put(bytes_data)
+                    content = content[512:]  
+        self.gcode.respond_info("Load print data done. Data size:%s"%(self.data_queue.qsize()))
+    
+    #喷头驱动主使能 禁用
+    def cmd_HEAD_DISABLE(self,gcmd):
+        self.cmd_queue.put(b'\x10')
+        self.print_done=True
+        self.head_en=False  
+        self.jet_en=False 
+        time.sleep(0.5)
+        with self.process_exit.get_lock():
+            self.process_exit.value=1
+        self.gcode.respond_info("Head Disable")
+    #喷墨使能 启用
+    def cmd_JET_ENABLE(self,gcmd):
+        self.cmd_queue.put(b'\x13')  
+        self.jet_en=True 
+        self.gcode.respond_info("Jet Enable")
+    #喷墨使能 禁用
+    def cmd_JET_DISABLE(self,gcmd):
+        self.cmd_queue.put(b'\x11')  
+        self.jet_en=False    
+        self.gcode.respond_info("Jet Disable") 
 
-    # TEST
-    def _print(self):
-        while self.print_data_transfer:
-            self.gcode.respond_info("TEST loop")
-            time.sleep(5)
-    def cmd_TEST1(self,gcmd):
-        self.gcode.respond_info("start thread")
-        self.print_data_transfer=True
-        self.test_thread.start()
+    def _verify_parity(self,data_byte):
         
+        data_bits = data_byte[:-1]  # 提取前4位数据位
+        parity_bit = int(data_byte[-1])  # 提取最低位奇偶校验位
+        calculated_parity  = 0
+        for bit in data_bits:
+            calculated_parity  ^= int(bit)
+        if calculated_parity == parity_bit:
+            return True
+        else:
+            return False
+
+    def _clear_queue(self,queue):
+        while not queue.empty():
+            queue.get()
+    def _serial_process(self,cmd_queue,data_queue,process_exit):
+        fifo_full=False
+        ser=serial.Serial(port='/dev/ttyCH9344USB3',baudrate=6000000,bytesize=serial.FIVEBITS,parity=serial.PARITY_EVEN,stopbits=serial.STOPBITS_ONE,timeout=0,)
+        while True: 
+            if process_exit.value==1:
+                ser.close()
+                break        
+            while not cmd_queue.empty():
+                cmd = cmd_queue.get()
+                ser.write(bytes(cmd))
+                self.gcode.respond_info("Send cmd:%s"%(cmd))
+
+            #读取串口返回数据   
+            if ser.in_waiting>0: #循环读出，直到获得最新数据
+                rev = ser.read(ser.in_waiting)
+                if rev[-1]==5 or rev[-1]==6:   #即接收数据00101 00110，不关心tx奇偶校验结果
+                    fifo_full=True
+                    self.gcode.respond_info("FIFO full.")
+                else:
+                    fifo_full=False
+                self.child_conn.send(fifo_full)
+            #如果fifo没满则发送多个数据           
+            if fifo_full==False:
+                queue_size=data_queue.qsize()
+                if queue_size>0:
+                    d=data_queue.get()
+                    ser.write(d)
+                    self.gcode.respond_info("send data: %d"%len(d))     
+
+
+    def cmd_TEST1(self,gcmd):
+        process = multiprocessing.Process(target=self._serial_process, args=(self.ser,self.cmd_queue, self.data_queue,self.process_exit,))
+        self.gcode.respond_info("process_exit:%s"%(str(self.process_exit.value)))
+        process.daemon=True
+        process.start()
+
+            
 
     def cmd_TEST2(self,gcmd):
-        self.gcode.respond_info("stop thread")
-        self.print_data_transfer=False
+        self.cmd_queue.put(b'\x11')
+        self.cmd_queue.put(b'\x10')
+        
+
+
 
 
     def cmd_TEST3(self,gcmd):
-        self.gcode.respond_info("creat thread")
-        self.test_thread=threading.Thread(target=self._print)
+        with self.process_exit.get_lock():
+            self.process_exit.value=1
+
+
 
         
 
 def load_config(config):
         return Xaar1003(config)
+
+
+# PRINT_INIT  ;
+# LOAD_CONFIG PATH=test/config.txt;
+# LOAD_PRINT_DATA PATH=test/data.txt;

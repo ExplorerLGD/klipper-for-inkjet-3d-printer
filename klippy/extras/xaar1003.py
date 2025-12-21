0ffe2b4b55c5a9eee7680170144dfd7d4090d142
ExplorerLGD <33108693+ExplorerLGD@users.noreply.github.com>
2025-12-21 18:30:56 +0800

    Add function to load zip file data

--- klippy/extras/xaar1003.py
+++ klippy/extras/xaar1003.py
@@ -1,5 +1,4 @@
-#gpiod 需要创建用户组，创建规则文件及命令，并将当前用户添加
-#FPGA USB也需要添加用户组sudo usermod -aG dialout $USER
+
 
 import logging,os
 import time
@@ -17,7 +16,6 @@ from pynq import allocate
 from pynq import MMIO
 from pynq import PL
 from pynq import DefaultHierarchy
-from pynq.lib.video import *
 import numpy as np
 
 class Xaar1003:
@@ -30,27 +28,37 @@ class Xaar1003:
         #----------overlay--------------------
         PL.reset()
         self.ol = Overlay("/home/xilinx/zynq.bit")
-        #self.ol.download("/home/xilinx/pl.dtbo")
-        self.ch1 = self.ol.axi_dma_1
-        self.ch1_buff=allocate(shape=(10*1024*1024,), dtype=np.uint8)#预分配10M缓冲区
-        self.channels = [self.ch1]
+        self.ch1 = self.ol.channel1.axi_dma_0
+        self.ch2 = self.ol.channel2.axi_dma_0
+        self.ch3 = self.ol.channel3.axi_dma_0
+        self.ch4 = self.ol.channel4.axi_dma_0
+        self.ch5 = self.ol.channel5.axi_dma_0
+        self.ch6 = self.ol.channel6.axi_dma_0
+        self.ch7 = self.ol.channel7.axi_dma_0
+        self.ch1_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)#预分配5M缓冲区
+        self.ch2_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)
+        self.ch3_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)
+        self.ch4_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)
+        self.ch5_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)
+        self.ch6_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)
+        self.ch7_buff=allocate(shape=(5*1024*1024,), dtype=np.uint8)
+        self.channels = [self.ch1, self.ch2, self.ch3, self.ch4, self.ch5, self.ch6, self.ch7]
+        self.ch_buffs = [self.ch1_buff, self.ch2_buff, self.ch3_buff, self.ch4_buff, self.ch5_buff, self.ch6_buff, self.ch7_buff]
         self.head_en = GPIO(GPIO.get_gpio_pin(0), 'out')
         self.head_dir = GPIO(GPIO.get_gpio_pin(1), 'out')
         self.head_jet = GPIO(GPIO.get_gpio_pin(2), 'out')
         self.jet_delay_time = MMIO(0x40000000, 0x1000) #Delay模块基址为0x40000000，大小为4K字节
-        self.active_channels = 0
         #---------gcode------------------- 
         self.gcode.register_command('PRINT_INIT',self.cmd_PRINT_INIT)
         self.gcode.register_command('SET_JET_DELAY',self.cmd_SET_JET_DELAY)
-        self.gcode.register_command('LOAD_CONFIG',self.cmd_LOAD_CONFIG)
-        self.gcode.register_command('WAIT',self.cmd_WAIT)
-        self.gcode.register_command('LOAD_PRINT_DATA',self.cmd_LOAD_PRINT_DATA)
+        self.gcode.register_command('LOAD_SINGLE_CONFIG',self.cmd_LOAD_SINGLE_CONFIG)
+        self.gcode.register_command('LOAD_SINGLE_PRINT_DATA',self.cmd_LOAD_SINGLE_PRINT_DATA)
+        self.gcode.register_command('LOAD_ZIP_CONFIG',self.cmd_LOAD_ZIP_CONFIG)
+        self.gcode.register_command('LOAD_ZIP_PRINT_DATA',self.cmd_LOAD_ZIP_PRINT_DATA)
 
-        #self.gcode.register_command('JET_STATE',self.cmd_JET_STATE)
         self.gcode.register_command('JET_ENABLE',self.cmd_JET_ENABLE)
         self.gcode.register_command('JET_DISABLE',self.cmd_JET_DISABLE)
         self.gcode.register_command('HEAD_DISABLE',self.cmd_HEAD_DISABLE)
-        #self.gcode.register_command('WAIT_ENCODER',self.cmd_WAIT_ENCODER)
 
         self.gcode.register_command('TEST1',self.cmd_TEST1)
         self.gcode.register_command('TEST2',self.cmd_TEST2)
@@ -65,10 +73,7 @@ class Xaar1003:
                 return content
 
     def cmd_PRINT_INIT(self,gcmd):
-        # 获取参数，如果没有提供，则使用默认值1
-        self.active_channels = gcmd.get_int('CHANNEL_NUM', 1)
         self.ch1.sendchannel.start()
-        self.ch1.recvchannel.start()
         self.head_en.write(1)
         time.sleep(0.5)
         self.head_en.write(0)
@@ -81,11 +86,12 @@ class Xaar1003:
         delay_value = int(delay_seconds * clock_freq)
         self.jet_delay_time.write(0x0, delay_value)
         self.gcode.respond_info(f"Set jet delay time:{delay_seconds}.")
-    def cmd_LOAD_CONFIG(self,gcmd):
-        # file_name = gcmd.get('FILE')
-        # directory = os.path.dirname(self.virtual_sdcard.current_file.name)
-        # config_file_path = os.path.join(directory, file_name)
-        with open("/home/xilinx/printer_data/gcodes/Config.txt", "r") as f:
+    def cmd_LOAD_SINGLE_CONFIG(self,gcmd):
+        file_name = gcmd.get('FILE')
+        channel_index = gcmd.get_int('CHANNEL',1)
+        directory = os.path.dirname(self.virtual_sdcard.current_file.name)
+        config_file_path = os.path.join(directory, file_name)
+        with open(config_file_path, "r") as f:
             hex_str_config = f.read()
             config_bytes = [
                 (int(hex_str_config[i+1], 16) << 4) | int(hex_str_config[i], 16)
@@ -94,27 +100,68 @@ class Xaar1003:
             config_array = np.array(config_bytes, dtype=np.uint8)
             dma_buffer = allocate(shape=config_array.shape, dtype=np.uint8)
             np.copyto(dma_buffer, config_array)
-            for i in range(self.active_channels):
-                self.channels[i].sendchannel.transfer(dma_buffer)
-                self.channels[i].sendchannel.wait()
+            self.channels[channel_index-1].sendchannel.transfer(dma_buffer)
+            self.channels[channel_index-1].sendchannel.wait()
             del dma_buffer
-        #self.gcode.respond_info("Loading config:%s" % (file_name)) 
-        self.gcode.respond_info("Config finish.")
+        self.gcode.respond_info("Loading config:%s" % (file_name)) 
 
-    def cmd_LOAD_PRINT_DATA(self,gcmd):
-        if(self.ch1.sendchannel.idle):
-            with open("/home/xilinx/printer_data/gcodes/1.txt", "r") as f:
+    def cmd_LOAD_SINGLE_PRINT_DATA(self,gcmd):
+        file_name = gcmd.get('FILE')
+        channel_index = gcmd.get_int('CHANNEL',1)
+        directory = os.path.dirname(self.virtual_sdcard.current_file.name)
+        data_file_path = os.path.join(directory, file_name)
+        if(self.channels[channel_index-1].sendchannel.idle):
+            with open(data_file_path, "r") as f:
                 hex_str_config = f.read()
                 data_bytes = [
                     (int(hex_str_config[i+1], 16) << 4) | int(hex_str_config[i], 16)
                     for i in range(0, len(hex_str_config), 2)
                 ]
                 data_array = np.array(data_bytes, dtype=np.uint8)
-                np.copyto(self.ch1_buff[:len(data_array)], data_array)
-                self.ch1.sendchannel.transfer(self.ch1_buff, nbytes=len(data_array))
+                np.copyto(self.ch_buffs[channel_index-1][:len(data_array)], data_array)
+                self.channels[channel_index-1].sendchannel.transfer(self.ch_buffs[channel_index-1], nbytes=len(data_array))
             self.gcode.respond_info("Load print data finish.")
         else:
             self.gcode.respond_info("DMA channel busy,load print data fail.")
+
+    def cmd_LOAD_ZIP_CONFIG(self,gcmd):
+        # 获取参数，如果没有提供，则使用默认值1
+        ch_index = gcmd.get_int('CHANNEL',1)
+        zip_path = self.virtual_sdcard.current_zip_file
+        with zipfile.ZipFile(zip_path, 'r') as zf:
+            file_path = f'CH{ch_index}/Config.txt'
+            with zf.open(file_path) as f:
+                hex_str_config = f.read().decode('utf-8')
+                config_bytes = [
+                    (int(hex_str_config[j+1], 16) << 4) | int(hex_str_config[j], 16)
+                    for j in range(0, len(hex_str_config), 2)
+                ]
+                config_array = np.array(config_bytes, dtype=np.uint8)
+                dma_buffer = allocate(shape=config_array.shape, dtype=np.uint8)
+                np.copyto(dma_buffer, config_array)
+                self.channels[ch_index-1].sendchannel.transfer(dma_buffer)
+                self.channels[ch_index-1].sendchannel.wait()
+                del dma_buffer
+        self.gcode.respond_info(f"Channel {ch_index} config finish.")
+
+    def cmd_LOAD_ZIP_PRINT_DATA(self,gcmd):
+        # 获取参数，如果没有提供，则使用默认值1
+        ch_index = gcmd.get_int('CHANNEL',1)
+        layer_index = gcmd.get_int('LAYER',1)
+        zip_path = self.virtual_sdcard.current_zip_file
+        with zipfile.ZipFile(zip_path, 'r') as zf:
+            file_path = f'CH{ch_index}/{layer_index}.txt'
+            with zf.open(file_path) as f:
+                hex_str_config = f.read().decode('utf-8')
+                data_bytes = [
+                    (int(hex_str_config[j+1], 16) << 4) | int(hex_str_config[j], 16)
+                    for j in range(0, len(hex_str_config), 2)
+                ]
+                data_array = np.array(data_bytes, dtype=np.uint8)
+                np.copyto(self.ch1_buff[:len(data_array)], data_array)
+                self.channels[ch_index-1].sendchannel.transfer(self.ch1_buff, nbytes=len(data_array))
+                self.channels[ch_index-1].sendchannel.wait()
+        self.gcode.respond_info(f"Channel {ch_index} layer {layer_index} load finish.")
 
     def cmd_JET_ENABLE(self,gcmd):
         self.head_jet.write(1)
@@ -122,132 +169,53 @@ class Xaar1003:
 
     def cmd_JET_DISABLE(self,gcmd):
         self.head_jet.write(0)
-        self.ch1.sendchannel.stop()
-        self.ch1.recvchannel.stop()
+        # self.ch1.sendchannel.stop()
+        # self.ch1.recvchannel.stop()
         self.gcode.respond_info("Jet disable.")
 
     def cmd_HEAD_DISABLE(self,gcmd):
         self.head_en.write(0)
         self.head_jet.write(0)
-        self.active_channels=0
         self.gcode.respond_info("Head disable.")
 
-    def cmd_WAIT(self,gcmd):
-        time = gcmd.get_float('TIME')
-        self.reactor.pause(self.reactor.monotonic()+time)
-
     def cmd_TEST1(self,gcmd):
-        try:
-            video = self.ol.video
-        except Exception as e:
-            self.gcode.respond_info(f"No self.ol.video: {e}")
-            return
+        # file_name = gcmd.get('FILE')
+        zip_path = self.virtual_sdcard.current_zip_file
+        self.gcode.respond_info(zip_path)
+        #config_file_path = os.path.join(zip_path, "CH1/Config.txt")
+        with zipfile.ZipFile(zip_path, 'r') as zf:
+            # 收集顶层目录名（只取第一个 path component）
+            folders = set()
+            for name in zf.namelist():
+                if not name:
+                    continue
+                first = name.rstrip('/').split('/', 1)[0]
+                if first:
+                    folders.add(first)
 
-        try:
-            ips = sorted(self.ol.ip_dict.keys())
-            self.gcode.respond_info("Overlay IPs: " + (", ".join(ips) if ips else "<none>"))
-
-            # video object overview
-            video_attrs = [a for a in dir(video) if not a.startswith("_")]
-            self.gcode.respond_info("ol.video attrs: " + (", ".join(video_attrs)))
-
-            # check ol.video.axi_vdma
-            has_axi_vdma = hasattr(video, "axi_vdma")
-            axi_vdma = getattr(video, "axi_vdma", None)
-            self.gcode.respond_info(f"ol.video.axi_vdma present: {has_axi_vdma}, value: {repr(axi_vdma)}")
-            if axi_vdma is not None:
-                self.gcode.respond_info("axi_vdma attrs: " + ", ".join([a for a in dir(axi_vdma) if not a.startswith("_")]))
-            # inspect hdmi_out and its internal _vdma
-            hdmi_out = getattr(video, "hdmi_out", None)
-            self.gcode.respond_info(f"hdmi_out: {repr(hdmi_out)}")
-            hdmi_vdma = getattr(hdmi_out, "_vdma", None) if hdmi_out is not None else None
-            self.gcode.respond_info(f"hdmi_out._vdma: {repr(hdmi_vdma)}")
-            if hdmi_vdma is not None:
-                self.gcode.respond_info("hdmi_out._vdma attrs: " + ", ".join([a for a in dir(hdmi_vdma) if not a.startswith("_")]))
-
-            # final helpful context
-            self.gcode.respond_info(f"Overlay repr: {repr(self.ol)}")
-
-        except Exception as ex:
-            self.gcode.respond_info(f"Error during TEST1 diagnostics: {ex}")
+            if not folders:
+                self.gcode.respond_info("No CH directories found in zip")
+            else:
+                for d in folders:
+                    self.gcode.respond_info(d)
+            target = None
+            for name in zf.namelist():
+                if name.lower().endswith('ch1/config.txt'):
+                    target = name
+                    break
+            if target is not None:
+                with zf.open(target) as f:
+                    content = f.read().decode('utf-8')
+                    self.gcode.respond_info(content)
+            else:
+                self.gcode.respond_info("Config.txt not found in CH1 folder.")
+        self.gcode.respond_info("TEST1 complete ")
 
     def cmd_TEST2(self,gcmd):
-        hdmi_out = self.ol.video.hdmi_out
-        Mode=VideoMode(1280, 720, 24)
-        hdmi_out.configure(Mode,PIXEL_BGR)
-        hdmi_out.start()
-        numframes = 600
-        for _ in range(numframes):
-            outframe = hdmi_out.newframe()
-            outframe.fill(1)
-            hdmi_out.writeframe(outframe)
-        hdmi_out.stop()
-        hdmi_out.close()
-        self.gcode.respond_info("TEST2 VDMA write complete ")
-
-
-
+        self.gcode.respond_info("TEST2 complete ")
 
     def cmd_TEST3(self,gcmd):
-        
-        
-        self.gcode.respond_info("TEST3 ")
-        ol = Overlay("/home/xilinx/zynq.bit")
-        ch1 = ol.axi_dma_1
-        head_en = GPIO(GPIO.get_gpio_pin(0), 'out')
-        head_dir = GPIO(GPIO.get_gpio_pin(1), 'out')
-        head_jet = GPIO(GPIO.get_gpio_pin(2), 'out')
-        mmio = MMIO(0x40000000, 0x1000)  # 0x1000为4K字节，和Vivado中显示的Range一致
-        mmio.write(0x0, 20000000) #20MHz，延迟1S
-        
-#        file_name = "config.txt"
-#        directory = os.path.dirname(self.virtual_sdcard.sdcard_dirname)
-#        file_path = os.path.join(directory,file_name)
-#        self.gcode.respond_info("Loading print data:%s"%(file_path)) 
-        
-        # 读取并解析txt文件
-        # 读取并解析Config.txt
-        with open("/home/xilinx/printer_data/gcodes/Config.txt", "r") as f:
-            hex_str_config = f.read()
-        byte_list = [
-            (int(hex_str_config[i+1], 16) << 4) | int(hex_str_config[i], 16)
-            for i in range(0, len(hex_str_config), 2)
-        ]
-        # 读取并解析1.txt
-        with open("/home/xilinx/printer_data/gcodes/1.txt", "r") as f:
-            hex_str_print = f.read()
-        self.gcode.respond_info(f'hex_str_print length: {len(hex_str_print)}')
-        printData_list = [
-            (int(hex_str_print[i+1], 16) << 4) | int(hex_str_print[i], 16)
-            for i in range(0, len(hex_str_print), 2)
-        ]
-        # 合并两个列表
-        merged_list = byte_list + printData_list
-        data_array = np.array(merged_list, dtype=np.uint8)
-        ch1_buffer = allocate(shape=data_array.shape, dtype=np.uint8)
-        np.copyto(ch1_buffer, data_array)
-        
-        head_jet.write(0)
-        time.sleep(2)
-        head_en.write(1)
-        time.sleep(0.5)
-        head_en.write(0)
-        time.sleep(0.5)
-        head_en.write(1)
-        
-        
-        
-        # 启动 DMA 传输
-        ch1.sendchannel.transfer(ch1_buffer)
-        ch1.sendchannel.wait()
-    
-        
-        time.sleep(5)
-        head_jet.write(1)
-        time.sleep(50)
-        head_en.write(0)
-        del ch1_buffer
-        self.gcode.respond_info("DMA send complete")
+        self.gcode.respond_info("TEST3 complete ")
 
 def load_config(config):
         return Xaar1003(config)
